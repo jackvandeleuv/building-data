1ed9c6f95873cfdc2f6864bef22bfe71ad61aad6
Jack Vandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2025-07-03 14:18:26 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const URI = 'https://jackvandeleuv.github.io/building_data';
+export const URI = 'https://jackvandeleuv.github.io/building-data';
 // export const URI = 'http://localhost:8000';
